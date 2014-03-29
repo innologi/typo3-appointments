698e770851a86ca7e1513979cd2fba7619c46a95
Frenck Lutke <git@innologi.nl>
2014-03-29 10:50:24 +0100

    [TASK] Make FormFieldValueValidator 6.2-compatible
    The FormFieldValueValidator isnt compatible because the default
    validators it employs have changed:
    - they dont return TRUE/FALSE
    - there is finally consistency: only NotEmpty does not allow empty
    values! :)
    
    While this extension stays compatible with 4.5 - 6.2 until we switch to
    the rewrittenPropertyMapper, it also needs to be consistent between
    versions: so also add NotEmpty if any of the other validators is picked.
    
    Change-Id: I20cd18a961fe58a09556fd3aeeaf1c7ce1acaee5

--- Classes/Domain/Validator/FormFieldValueValidator.php
+++ Classes/Domain/Validator/FormFieldValueValidator.php
@@ -65,62 +65,86 @@ class Tx_Appointments_Domain_Validator_FormFieldValueValidator extends Tx_Appoin
 		$validationTypes = t3lib_div::trimExplode(',', $formField->getValidationTypes(), TRUE);
 		$validatorResolver = $this->objectManager->get('Tx_Extbase_Validation_ValidatorResolver');
 		$validatorConjunction = $this->objectManager->get('Tx_Extbase_Validation_Validator_ConjunctionValidator');
+		$addNotEmpty = FALSE;
 
-		foreach ($validationTypes as $validationType) {
-			$validationType = intval($validationType);
-			#@TODO currently tells FE the field is required if ANY of these is selected, but alphanum, string and text don't require it to be, so they're disabled in TCA. Fix this as soon as extbase is consistent @ its Validators.
-			switch ($validationType) {
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_ALPHANUMERIC: #@LOW move these outside of formfield and into the validator to lose the formfield dependence
-					$validator = $validatorResolver->createValidator('Alphanumeric');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_DATE_TIME:
-					$validator = $validatorResolver->createValidator('DateTime');
-					//$value is of type string, while this validator tests it for objecttype DateTime
-					if (!empty($value)) {
-						try {
-							$newValue = new DateTime($value);
-							//results in NULL if a valid DateTime string but not in the specified format, so that you can't get away with a timestamp or some other format
-							$value = ($value === $newValue->format('d-m-Y')) ? $newValue : NULL;
-						} catch (Exception $e) { //if $value is no valid DateTime string
-							$value = NULL;
+		if (!empty($validationTypes)) {
+			$addNotEmpty = TRUE;
+			foreach ($validationTypes as $validationType) {
+				$validationType = intval($validationType);
+				#@TODO currently tells FE the field is required if ANY of these is selected, but alphanum, string and text don't require it to be, so they're disabled in TCA. Fix this as soon as extbase is consistent @ its Validators.
+				switch ($validationType) {
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_ALPHANUMERIC: #@LOW move these outside of formfield and into the validator to lose the formfield dependence
+						$validator = $validatorResolver->createValidator('Alphanumeric');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_DATE_TIME:
+						$validator = $validatorResolver->createValidator('DateTime');
+						//$value is of type string, while this validator tests it for objecttype DateTime
+						if (!empty($value)) {
+							try {
+								$newValue = new DateTime($value);
+								//results in NULL if a valid DateTime string but not in the specified format, so that you can't get away with a timestamp or some other format
+								$value = ($value === $newValue->format('d-m-Y')) ? $newValue : NULL;
+							} catch (Exception $e) { //if $value is no valid DateTime string
+								$value = NULL;
+							}
+							if ($value === NULL && version_compare(TYPO3_branch, '6.2', '>=')) {
+								// something other than DateTime is good enough
+								$value = 1;
+							}
 						}
-					}
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_EMAIL_ADDRESS:
-					$validator = $validatorResolver->createValidator('EmailAddress');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_FLOAT:
-					$validator = $validatorResolver->createValidator('Float');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_INTEGER:
-					$validator = $validatorResolver->createValidator('Integer');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_NOT_EMPTY:
-					$validator = $validatorResolver->createValidator('NotEmpty');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_NUMBER:
-					$validator = $validatorResolver->createValidator('Number');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_STRING:
-					$validator = $validatorResolver->createValidator('String');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_TEXT:
-					$validator = $validatorResolver->createValidator('Text');
-					break;
-				case Tx_Appointments_Domain_Model_FormField::VALIDATE_NATURALNUMBER:
-					//without options, it sets startRange = 0 and endRange = PHP_INT_MAX
-					$validator = $validatorResolver->createValidator('NumberRange');
-					break;
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_EMAIL_ADDRESS:
+						$validator = $validatorResolver->createValidator('EmailAddress');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_FLOAT:
+						$validator = $validatorResolver->createValidator('Float');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_INTEGER:
+						$validator = $validatorResolver->createValidator('Integer');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_NOT_EMPTY:
+						$validator = $validatorResolver->createValidator('NotEmpty');
+						$addNotEmpty = FALSE;
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_NUMBER:
+						$validator = $validatorResolver->createValidator('Number');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_STRING:
+						$validator = $validatorResolver->createValidator('String');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_TEXT:
+						$validator = $validatorResolver->createValidator('Text');
+						break;
+					case Tx_Appointments_Domain_Model_FormField::VALIDATE_NATURALNUMBER:
+						//without options, it sets startRange = 0 and endRange = PHP_INT_MAX
+						$validator = $validatorResolver->createValidator('NumberRange');
+						break;
+				}
+				$validatorConjunction->addValidator($validator);
 			}
-			$validatorConjunction->addValidator($validator);
 		}
 
-		if ($validatorConjunction->isValid($value)) {
-			return TRUE;
+		if (version_compare(TYPO3_branch, '6.2', '<')) {
+			if ($validatorConjunction->isValid($value)) {
+				return TRUE;
+			}
+			$errors = $validatorConjunction->getErrors();
+		} else {
+			#@TODO remove this crap once we go 6.2-only
+			// temporary change to enforce consistency between TYPO3 versions
+			if ($addNotEmpty) {
+				$validatorConjunction->addValidator($validatorResolver->createValidator('NotEmpty'));
+			}
+
+			$result = $validatorConjunction->validate($value);
+			if (!$result->hasErrors()) {
+				return TRUE;
+			}
+			$errors = $result->getErrors();
 		}
 
 		$propertyError = new Tx_Extbase_Validation_PropertyError('value');
-		$propertyError->addErrors($validatorConjunction->getErrors());
+		$propertyError->addErrors($errors);
 		$this->errors[] = $propertyError;
 		return FALSE;
 	}
