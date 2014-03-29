3152596be72ac5304be80842166cf525fd5f353b
Frenck Lutke <git@innologi.nl>
2014-03-29 11:04:22 +0100

    [TASK] Raise supported TYPO3 versions
    Change-Id: I67ee87a572671a3716ffee3498c14c44056dd689

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,9 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.9.9',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '1.3',
-			'fluid' => '1.3',
-			'typo3' => '4.5.0-6.1.99',
+			'typo3' => '4.5.0-6.2.99',
 			'tt_address' => '2.2.1',
 			'vhs' => '',
 		),
